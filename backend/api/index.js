16415fd990cc4c1ab38777f457d932a90166a112
Zahid Gazi <123141921+1mnuvq@users.noreply.github.com>
2025-04-12 11:37:05 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,12 +3,13 @@ const otpRoutes = require("./routes/otp");
 const userRoutes = require("./routes/user");
 const cors = require("cors");
 
+
+const app = express();
+
 app.use(cors({
   origin: "*",
   credentials: true 
 }));
-
-const app = express();
 app.use(express.json());
 
 
